c5ecd24f5dd9c845cdb6bc4592a1c3b46598a610
Sharif Olorin <sio@tesser.org>
2014-12-26 17:20:59 +1100

    Get a password sans echo

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import System.IO
+import Control.Exception
+
+getPassword :: IO String
+getPassword = getPassword' >>= passThroughAction (putStrLn "")
+  where
+    getPassword' =     putStr "Seed: "
+                   >>  hFlush stdout
+                   >>  hSetEcho stdin False
+                   >>  getLine
+                   >>= return
+
+    passThroughAction act x = act >> return x
+
 main :: IO ()
-main = error "NYI"
+main = do
+    pass <- getPassword
+    putStrLn pass
