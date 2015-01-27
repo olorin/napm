0261c284242270125ceb7b1a165adbde41caa204
Sharif Olorin <sio@tesser.org>
2015-01-27 10:15:54 +0000

    Basic IO with defaults

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,23 @@ module Main where
 import           Control.Applicative
 import           Control.Exception
 import           Control.Monad
-import qualified Crypto.Hash.SHA512  as SHA
+import qualified Crypto.Hash.SHA512     as SHA
+import qualified Data.ByteString.Base64 as B64
 import           Data.Monoid
-import           Data.Text           (Text)
-import qualified Data.Text           as T
+import           Data.Text              (Text)
+import qualified Data.Text              as T
 import           Data.Text.Encoding
+import qualified Data.Text.IO           as TIO
 import           System.Directory
 import           System.IO
 
 readPassphrase :: IO Text
-readPassphrase = readPassphrase' >>= passThroughAction (putStrLn "")
+readPassphrase = readPassphrase' >>= passThroughAction (hPutStrLn stderr "" >> hSetEcho stdin True)
   where
-    readPassphrase' = liftM T.pack $ putStr "Seed: "
-                   >>  hFlush stdout
-                   >>  hSetEcho stdin False
-                   >>  getLine
+    readPassphrase' = hPutStr stderr "Seed: "
+                   >> hFlush stderr
+                   >> hSetEcho stdin False
+                   >> TIO.hGetLine stdin
 
     passThroughAction act x = act >> return x
 
@@ -26,9 +28,13 @@ napmDataDir = try $ getAppUserDataDirectory "napm"
 
 computePassword :: Int -> Text -> Text -> Text
 computePassword len passphrase context =
-    T.take len . decodeUtf8 $ SHA.hash salted
+    T.take len . decodeUtf8 . B64.encode $ SHA.hash salted
   where
     salted = encodeUtf8 passphrase <> encodeUtf8 context
 
 main :: IO ()
-main = error "NYI"
+main = do
+    pp <- readPassphrase
+    hPutStr stderr "Context: "
+    ctx <- TIO.hGetLine stdin
+    TIO.hPutStr stdout $ computePassword 12 pp ctx
