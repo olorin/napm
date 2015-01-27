fd802054e5b87442f46febca3760aca3ff8d09f3
Sharif Olorin <sio@tesser.org>
2015-01-27 09:54:58 +0000

    Hash some things

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,18 @@ module Main where
 import           Control.Applicative
 import           Control.Exception
 import           Control.Monad
+import qualified Crypto.Hash.SHA512  as SHA
+import           Data.Monoid
 import           Data.Text           (Text)
 import qualified Data.Text           as T
+import           Data.Text.Encoding
 import           System.Directory
 import           System.IO
 
-getPassword :: IO Text
-getPassword = getPassword' >>= passThroughAction (putStrLn "")
+readPassphrase :: IO Text
+readPassphrase = readPassphrase' >>= passThroughAction (putStrLn "")
   where
-    getPassword' = liftM T.pack $ putStr "Seed: "
+    readPassphrase' = liftM T.pack $ putStr "Seed: "
                    >>  hFlush stdout
                    >>  hSetEcho stdin False
                    >>  getLine
@@ -20,6 +23,12 @@ getPassword = getPassword' >>= passThroughAction (putStrLn "")
 
 napmDataDir :: IO (Either SomeException FilePath)
 napmDataDir = try $ getAppUserDataDirectory "napm"
+
+computePassword :: Int -> Text -> Text -> Text
+computePassword len passphrase context =
+    T.take len . decodeUtf8 $ SHA.hash salted
+  where
+    salted = encodeUtf8 passphrase <> encodeUtf8 context
 
 main :: IO ()
 main = error "NYI"
