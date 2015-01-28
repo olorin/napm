b0f81c967e97e6eef1bec0c3a0dd97d287d153af
Sharif Olorin <sio@tesser.org>
2015-01-28 07:25:26 +0000

    Test length of generated passwords

--- tests/NapmTest.hs
+++ tests/NapmTest.hs
@@ -2,8 +2,11 @@
 
 module Main where
 
+import           Control.Monad
 import           Data.Monoid
 import           Data.Text     (Text)
+import qualified Data.Text     as T
+import           System.Random
 import           Test.Hspec
 
 import           Napm.Password
@@ -12,6 +15,18 @@ main :: IO ()
 main = hspec suite
 
 suite :: Spec
-suite = describe "computePassword" $
+suite = describe "computePassword" $ do
     it "hashes an alphanumeric passphrase and context" $
         computePassword 12 "s33d" "c0nt3xt" `shouldBe` "xwR3ziEmkGc7"
+
+    it "generates a password of the requested (short) length" $ do
+        l <- getRandomLength
+        s <- getRandomText 12
+        c <- getRandomText 12
+        T.length (computePassword l s c) `shouldBe` l
+
+getRandomLength :: IO Int
+getRandomLength = getStdRandom (randomR (1,50))
+
+getRandomText :: Int -> IO Text
+getRandomText n = liftM (T.pack . take n . randoms) getStdGen
