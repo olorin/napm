1ab84947c365fc8a43bf9e8a66adedd1d4bbfe80
Sharif Olorin <sio@tesser.org>
2015-01-29 15:18:49 +0000

    Read context file on startup

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,18 @@ module Main where
 import           Control.Applicative
 import           Control.Exception
 import           Control.Monad
+import           Control.Monad.Error
+import           Data.Bifunctor
 import           Data.Monoid
-import           Data.Text           (Text)
-import qualified Data.Text           as T
+import           Data.Text                    (Text)
+import qualified Data.Text                    as T
 import           Data.Text.Encoding
-import qualified Data.Text.IO        as TIO
+import qualified Data.Text.IO                 as TIO
 import           System.Directory
 import           System.IO
+import           Text.PrettyPrint.ANSI.Leijen (Doc)
 
+import           Napm.ContextFile
 import           Napm.Password
 
 readPassphrase :: IO Text
@@ -23,12 +27,29 @@ readPassphrase = readPassphrase' >>= passThroughAction (hPutStrLn stderr "" >> h
 
     passThroughAction act x = act >> return x
 
-napmDataDir :: IO (Either SomeException FilePath)
-napmDataDir = try $ getAppUserDataDirectory "napm"
+napmDataDir :: IO (Either String FilePath)
+napmDataDir = first show `fmap` (try $ getAppUserDataDirectory "napm" :: IO (Either SomeException FilePath))
+
+getContexts :: FilePath -> IO (Either String [PasswordContext])
+getContexts dataDir = doesFileExist napmContextFile >>= maybeParseContexts
+  where
+    napmContextFile = dataDir <> "/contexts"
+
+    maybeParseContexts False = do
+        hPutStrLn stderr "Can't find a context file. Proceeding without one."
+        return $ Right []
+    maybeParseContexts True = first show `fmap` parseContextFile napmContextFile
 
 main :: IO ()
 main = do
-    pp <- readPassphrase
-    hPutStr stderr "Context: "
-    ctx <- TIO.hGetLine stdin
-    TIO.hPutStr stdout $ computePassword 12 pp ctx
+    dataDir <- napmDataDir
+    contexts <- case dataDir of
+        Left e -> return $ Left e
+        Right d -> getContexts d
+    case contexts of
+        Left e -> hPutStrLn stderr e
+        Right _ -> do
+            pp <- readPassphrase
+            hPutStr stderr "Context: "
+            ctx <- TIO.hGetLine stdin
+            TIO.hPutStr stdout $ computePassword 12 pp ctx
